fe129fa167277f24c06ee73c2292cdd3b8a8bd74
JRoper18 <jackroper18@gmail.com>
2021-11-03 21:48:41 +0000

    better usefulness for l2

--- src/main/kotlin/grammars/lambda2/Lambda2Language.kt
+++ src/main/kotlin/grammars/lambda2/Lambda2Language.kt
@@ -19,6 +19,34 @@ open class Lambda2Language : Language {
         return ProgramGenerationResult(prog, examples, status)
     }
 
+    private fun splitProgramInputs(input : String) : List<String> {
+        val args = mutableListOf<String>()
+        var builtArg = ""
+        var depth = 0
+        for(ch in input) {
+            if(ch == '[') {
+                depth += 1
+            }
+            else if(ch == ']') {
+                depth -= 1
+            }
+            
+            if(ch == ',' && depth == 0) {
+                args.add(builtArg.toString())
+                builtArg = ""
+            }
+            else {
+                builtArg += ch
+            }
+        }
+        if(builtArg != "") {
+            args.add(builtArg)
+        }
+        return args.toList().map {
+            it.trim()
+        }
+    }
+
     override fun isProgramUseful(result: ProgramGenerationResult): Boolean {
         val examples = result.examples
         val outputs = examples.map {
@@ -29,16 +57,18 @@ open class Lambda2Language : Language {
             return false
         }
         // If we're the identity, that's not useful either. 
-       var isModifying = false
-       for(example in examples) {
-           if(example.first != example.second) {
-               isModifying = true
-               break
-           }
-       }
-       if(!isModifying) {
-           return false
-       }
+        var isModifying = false
+        for(example in examples) {
+            val inputs = splitProgramInputs(example.first).toSet()
+            // No identities and no returning unmodified inputs
+            if(example.first != example.second && !inputs.contains(example.second.trim())) {
+                isModifying = true
+                break
+            }
+        }
+        if(!isModifying) {
+            return false
+        }
         return result.examples.isNotEmpty() && result.status == ProgramGenerationResult.PROGRAM_STATUS.RUNNABLE
     }
 
