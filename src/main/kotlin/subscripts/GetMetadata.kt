8c6f49b9774bd0d079c300b904a39a8f21e1d45c
JRoper18 <jackroper18@gmail.com>
2022-02-01 02:20:21 +0000

    working on RL

--- src/main/kotlin/subscripts/GetMetadata.kt
+++ src/main/kotlin/subscripts/GetMetadata.kt
@@ -14,29 +14,42 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.annotations.SerializedName
 import grammar.Symbol
+import java.io.PrintWriter
 
 data class LanguageMetadata(
     @SerializedName("symbols") val symbols : Array<Symbol>,
     @SerializedName("rules") val rules : Array<ProductionRule>
 )
 
-suspend fun <I, O> getMetadata(language : Language<I, O>) {
+suspend fun <I, O> getMetadata(
+    language : Language<I, O>,
+    outputStream: PrintWriter = PrintWriter(System.out, true)
+) {
     // We're going to put a json metadata of the language to stdout.
     val gson = GsonBuilder().setPrettyPrinting().create()
+    // val gson = Gson()
     val g = language.grammar()
     val metadata = LanguageMetadata(
         symbols = g.symbols.toTypedArray(),
         rules = g.rules.map {it.rule}.toTypedArray()
     )
     val jsonStr = gson.toJson(metadata)
-    println(jsonStr)
+    outputStream.println(jsonStr)
+    outputStream.flush()
 }
 
 suspend fun getMetadataCmd(args: Array<String>) {
     val parser = ArgParser("metadata")
     val lanChoice by parser.option(ArgType.Choice<LanguageRef>(), shortName = "l", description = "Input language to generate").required()
+    val outputLoc by parser.option(ArgType.String, fullName = "output", shortName = "o", description = "Output file name")
     parser.parse(args)
     val lan = argsToLanguage(lanChoice)
-    getMetadata(lan)
+    val outputStream : PrintWriter
+    if(outputLoc == null) {
+        outputStream = PrintWriter(System.out, true)
+    } else {
+        outputStream = PrintWriter(outputLoc)
+    }
+    getMetadata(lan, outputStream)
 
 }
